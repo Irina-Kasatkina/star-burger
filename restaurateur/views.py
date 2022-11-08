3e031796e9f6aa3f61656860bf851a7f7d3aa67b
Irina Kasatkina <kasatkina235@gmail.com>
2022-11-08 14:30:45 +0300

    Show manager distances to restaurants

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,9 +1,12 @@
+import requests
 from django import forms
+from django.conf import settings
 from django.contrib.auth import authenticate, login, views as auth_views
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
+from geopy import distance
 
 from foodcartapp.models import Order, OrderItem, Product, Restaurant, RestaurantMenuItem
 
@@ -106,15 +109,26 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url="restaurateur:login")
 def view_orders(request):
-    orders_items = OrderItem.objects.select_related("order", "product").exclude(order__status="CM").order_by("order__status", "order__id")
-    restaurants_menu_items = RestaurantMenuItem.objects.filter(availability=True).select_related("product", "restaurant")
+    orders_items = (
+        OrderItem.objects
+                 .select_related("order", "product")
+                 .exclude(order__status="CM")
+                 .order_by("order__status", "order__id")
+    )
+    restaurants_menu_items = (
+        RestaurantMenuItem.objects
+                          .filter(availability=True)
+                          .select_related("product", "restaurant")
+    )
 
     orders_for_page = []
     order = order_restaurants = None    
     for order_item in orders_items:
         if not order or order_item.order.id != order.id:
             if order:
-                orders_for_page.append(serialize_order(order, order_cost, order_restaurants))
+                orders_for_page.append(
+                    serialize_order(order, order_cost, get_restaurants_definitions(order.address, order_restaurants))
+                )
 
             order = order_item.order
             order_cost = order_item.price * order_item.quantity
@@ -125,8 +139,9 @@ def view_orders(request):
             order_restaurants = order_restaurants & get_order_item_restaurants(restaurants_menu_items, order_item)
 
     if order:
-        orders_for_page.append(serialize_order(order, order_cost, order_restaurants))
-    # print(orders_for_page)
+        orders_for_page.append(
+            serialize_order(order, order_cost, get_restaurants_definitions(order.address, order_restaurants))
+        )
 
     return render(
         request,
@@ -135,7 +150,41 @@ def view_orders(request):
     )
 
 
-def serialize_order(order, order_cost, restaurants_names):
+def get_restaurants_definitions(address, restaurants):
+    address_coordinates = fetch_coordinates(address)
+
+    restaurants_with_distances = []
+    for restaurant in restaurants:
+        restaurant_coordinates = fetch_coordinates(restaurant.address)
+        restaurant_distance = round(distance.distance(restaurant_coordinates, address_coordinates).km, 2)
+        restaurants_with_distances.append((restaurant.name, restaurant_distance))
+
+    restaurants_with_distances.sort(key=lambda x: x[1])
+    return [
+        f"{restaurant_name} - {restaurant_distance} км"
+        for restaurant_name, restaurant_distance in restaurants_with_distances
+    ]
+
+
+def fetch_coordinates(address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": settings.YANDEX_GEO_API_KEY,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lat, lon
+
+
+def serialize_order(order, order_cost, restaurants):
     return {
         "id": order.id,
         "status": order.get_status_display(),
@@ -147,7 +196,7 @@ def serialize_order(order, order_cost, restaurants_names):
         "comment": order.comment,
         "cost": order_cost,
         "restaurant": order.restaurant.name if order.restaurant else "",
-        "restaurants": sorted([restaurant_name for restaurant_name in restaurants_names])
+        "restaurants": restaurants,
     }
 
 
@@ -156,7 +205,7 @@ def get_order_item_restaurants(restaurants_menu_items, order_item):
         return set()
 
     return {
-        restaurants_menu_item.restaurant.name
+        restaurants_menu_item.restaurant
         for restaurants_menu_item in restaurants_menu_items
         if restaurants_menu_item.product.id == order_item.product.id
     }
